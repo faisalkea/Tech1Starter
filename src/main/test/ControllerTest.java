cdc26409954fe3b96041b10f9be6e99c563fc149
faisaljarkass <fafj@kea.dk>
2017-03-17 22:25:16 +0100

    Added maven jUnit test for ContentController.java

--- src/main/test/ControllerTest.java
+++ src/main/test/ControllerTest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.*;
 
 import main.controller.ContentController;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
 public class ControllerTest {
 
     @Test
